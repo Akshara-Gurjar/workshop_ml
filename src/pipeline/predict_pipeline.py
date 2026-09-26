043146dead3e6eff1b8903cc4f90c706071fe231
akshara gurjar <aksharag406@gmail.com>
2026-09-26 12:00:35 +0530

    pipeline_completed

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,84 @@
+import sys
+import os
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+from src.logger import get_logger
+logger = get_logger(__name__)
+
+class CustomData:
+    def __init__(self, age:int, gender:str , fever:float, cough:str , city:str) -> None:
+        self.age = age
+        self.gender = gender
+        self.fever = fever
+        self.cough = cough
+        self.city = city
+
+    def get_data_as_dataframe(self) -> None:
+        try:
+            custom_data_input_dict = {
+                "age": [self.age],
+                                "gender": [self.gender],
+                "fever": [self.fever],
+                "cough": [self.cough],
+                "city": [self.city]
+            }
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e :
+            raise CustomException(e,sys)
+
+class PredictPipeline:
+    def __init__(self) -> None:
+        self.model_path = os.path.join("artifacts", "model.pkl")
+        self.preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+
+    def predict(self, features: pd.DataFrame) -> None:
+        try:
+            logger.info("Loading Model and Preprocessor")
+
+            model = load_object(self.model_path)
+            preprocessor = load_object(self.preprocessor_path)
+
+            data_scaled = preprocessor.transform(features)
+
+            prediction = model.predict(data_scaled)
+
+            probability = None
+
+            if hasattr(model, "predict_proba"):
+                probability = round(
+                    max(model.predict_proba(data_scaled)[0]) * 100,
+                    2
+                )
+
+            result = "Positive" if int(prediction[0]) == 1 else "Negative"
+
+            logger.info(
+                f"Prediction completed: {result}, confidence_score = {probability}"
+            )
+
+            return result, probability
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+# IMPORTANT: This must be OUTSIDE the class
+if __name__ == "__main__":
+
+    data = CustomData(
+        age=25,
+        gender="Male",
+        fever=101.2,
+        cough="Strong",
+        city="Delhi"
+    )
+
+    final_data = data.get_data_as_dataframe()
+
+    predict_pipeline = PredictPipeline()
+
+    prediction, probability = predict_pipeline.predict(final_data)
+
+    print("Prediction:", prediction)
+    print("Confidence:", probability, "%")
